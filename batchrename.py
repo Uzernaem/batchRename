fb5a91c1db4db32ac96c15d190ace03917661898
Mark Wu <markwu.ca@gmail.com>
2019-08-08 01:11:33 -0400

    Added option to leave tier blank, testing how git works

--- batchrename.py
+++ batchrename.py
@@ -3,20 +3,24 @@ import time
 from datetime import datetime, timezone
 
 def main():
-    i = 0 #comment out this feature when not used in testing
+    #i = 0 #comment out this feature when not used in testing
 
-    userdir = input("What subfolder are your files in?")
-    tiertag = input("What tier tag would you like to add? (Specific to Dev's needs)")
+    userDir = input("What subfolder are your files in?")
+    tierTag = input("What tier tag would you like to add? (Specific to Dev's needs)")
+    #add custom file extensions by stripping last text from filename before the period
 
-    for filename in os.listdir(userdir): 
-        namestring = str(datetime.fromtimestamp(os.path.getctime(userdir + "/" + filename), timezone.utc))
+    for filename in os.listdir(userDir): 
+        namestring = str(datetime.fromtimestamp(os.path.getctime(userDir + "/" + filename), timezone.utc))
         namestring = ''.join(e for e in namestring if e.isalnum())
-        dst = namestring + str(i) + "tier" + tiertag + ".txt"
-        src = userdir + "/" + filename
-        dst = userdir + "/" + dst
+        if tierTag:
+            dst = namestring + str(i) + "tier" + tierTag + ".txt"
+        else:
+            dst = namestring + str(i) + "tier" + ".txt"
+        src = userDir + "/" + filename
+        dst = userDir + "/" + dst
 
         os.rename(src,dst)
-        i += 1
+        #i += 1
 
 if __name__ == '__main__':
     main()
